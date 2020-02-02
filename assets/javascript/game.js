cbc4ab326effdf8eb9ec5164803a3683cac8d462
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-02-02 14:07:39 -0800

    Updated game.js with section comments, cleaned syntax, removed commented logic unnecessary for game, edited camel case variables.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 // global variables 
+// =====================================================
 
 // variables and arrays for storing data
-var cookieoptions = ["c", "o", "s", "m", "p", "g", "f", "w"];
+var cookieOptions = ["c", "o", "s", "m", "p", "g", "f", "w"];
 
 // documentation
 var winScore = document.getElementById("wins");
@@ -9,13 +10,10 @@ var lossScore = document.getElementById("losses");
 var guessesRemain = document.getElementById("guessRemain");
 var lettersGuessed = document.getElementById("guessTally");
 var cookiePic = document.getElementById("cookieImage");
-var cookieChoosen = document.getElementById("cookieChoice");
+var cookieChosen = document.getElementById("cookieChoice");
 var monsterPic = document.getElementById("monsterImage");
 var monsterAnswer = document.getElementById("monsterReaction");
 var gameStatement = document.getElementById("outcome");
-
-
-
 
 // game counters
 var winCount = 0;
@@ -24,9 +22,8 @@ var guessesLeft = 4;
 var userGuessed = [];
 var monsterChoice = "";
 
-
-
 // functions to be used multiple times throughout the game - global functions
+// =============================================================================
 
 // start new game round
 function startGame() {
@@ -48,7 +45,7 @@ function restart() {
     userGuessed = [];
 
     cookiePic.setAttribute("src", "assets/images/questionmark.jpg");
-    cookieChoosen.textContent = "";
+    cookieChosen.textContent = "";
     monsterPic.setAttribute("src", "assets/images/chef-cookie.jpeg");
     monsterAnswer.textContent = "";
     gameStatement.textContent = "";
@@ -61,12 +58,12 @@ function restart() {
 
 // creates monsters choice
 function monsterAction() {
-
-    monsterChoice = cookieoptions[Math.floor(Math.random() * cookieoptions.length)];
-
+    monsterChoice = cookieOptions[Math.floor(Math.random() * cookieOptions.length)];
     console.log(monsterChoice);
 }
 
+// Game Logic
+// ====================================================
 
 // user makes guess 
 document.onkeyup = function (event) {
@@ -76,71 +73,44 @@ document.onkeyup = function (event) {
     console.log(userChoice);
 
     // state which user choices to look for 
-    if ((userChoice === "c") || (userChoice === "o") || (userChoice === "s") || (userChoice === "m") || (userChoice === "p") || (userChoice === "g") || (userChoice === "f") || (userChoice === "w")) {
-
-        // change statement and pic with each choice
-        // if (userChoice === "c") {
-        //     cookieChoosen.textContent = "You chose chocolate chip";
-        //     cookiePic.setAttribute("src", "assets/images/chocolatechip.jpeg");
-        // } else if (userChoice === "o") {
-        //     cookieChoosen.textContent = "You chose oatmeal";
-        //     cookiePic.setAttribute("src", "assets/images/oatmeal.jpeg");
-        // } else if (userChoice === "g") {
-        //     cookieChoosen.textContent = "You chose gingerbread";
-        //     cookiePic.setAttribute("src", "assets/images/gingerbread.jpeg");
-        // } else if (userChoice === "s") {
-        //     cookieChoosen.textContent = "You chose snickerdoodle";
-        //     cookiePic.setAttribute("src", "assets/images/snickerdoodle.jpeg");
-        // } else if (userChoice === "p") {
-        //     cookieChoosen.textContent = "You chose peanut butter";
-        //     cookiePic.setAttribute("src", "assets/images/pb.jpeg");
-        // } else if (userChoice === "f") {
-        //     cookieChoosen.textContent = "You chose fortune cookie";
-        //     cookiePic.setAttribute("src", "assets/images/fortune.jpeg");
-        // } else if (userChoice === "m") {
-        //     cookieChoosen.textContent = "You chose monster cookie";
-        //     cookiePic.setAttribute("src", "assets/images/monstercookie.jpeg");
-        // } else if (userChoice === "w") {
-        //     cookieChoosen.textContent = "You chose white chocolate chip";
-        //     cookiePic.setAttribute("src", "assets/images/whitechocolate.jpeg");
-        // }
+    if ((userChoice === "c") || (userChoice === "o") || (userChoice === "s") || (userChoice === "m") ||
+        (userChoice === "p") || (userChoice === "g") || (userChoice === "f") || (userChoice === "w")) {
 
         // switch to remove the extensive if/else statements
         switch (userChoice) {
             case "c":
-                cookieChoosen.textContent = "You chose chocolate chip";
+                cookieChosen.textContent = "You chose chocolate chip";
                 cookiePic.setAttribute("src", "assets/images/chocolatechip.jpeg");
                 break;
             case "o":
-                cookieChoosen.textContent = "You chose oatmeal";
+                cookieChosen.textContent = "You chose oatmeal";
                 cookiePic.setAttribute("src", "assets/images/oatmeal.jpeg");
                 break;
             case "g":
-                cookieChoosen.textContent = "You chose gingerbread";
+                cookieChosen.textContent = "You chose gingerbread";
                 cookiePic.setAttribute("src", "assets/images/gingerbread.jpeg");
                 break;
             case "s":
-                cookieChoosen.textContent = "You chose snickerdoodle";
+                cookieChosen.textContent = "You chose snickerdoodle";
                 cookiePic.setAttribute("src", "assets/images/snickerdoodle.jpeg");
                 break;
             case "p":
-                cookieChoosen.textContent = "You chose peanut butter";
+                cookieChosen.textContent = "You chose peanut butter";
                 cookiePic.setAttribute("src", "assets/images/pb.jpeg");
                 break;
             case "f":
-                cookieChoosen.textContent = "You chose fortune cookie";
+                cookieChosen.textContent = "You chose fortune cookie";
                 cookiePic.setAttribute("src", "assets/images/fortune.jpeg");
                 break;
             case "m":
-                cookieChoosen.textContent = "You chose monster cookie";
+                cookieChosen.textContent = "You chose monster cookie";
                 cookiePic.setAttribute("src", "assets/images/monstercookie.jpeg");
                 break;
             case "w":
-                cookieChoosen.textContent = "You chose white chocolate chip";
+                cookieChosen.textContent = "You chose white chocolate chip";
                 cookiePic.setAttribute("src", "assets/images/whitechocolate.jpeg");
                 break;
         }
-
 
         if (userChoice === monsterChoice) {
             winCount++;
@@ -158,7 +128,6 @@ document.onkeyup = function (event) {
             gameStatement.textContent = "Wrong cookie. Try again.";
 
         }
-
         if (guessesLeft === 0) {
             lossCount++;
             monsterPic.setAttribute("src", "assets/images/angrycookie.jpg");
@@ -167,20 +136,18 @@ document.onkeyup = function (event) {
             startGame();
             monsterAction();
         }
-
         if (lossCount === 10) {
             alert("You have lost 10 times. Cookie Monster says 'No More'! Would you like to play again?");
             restart();
         }
     }
 
-
     winScore.textContent = "Wins: " + winCount;
     lossScore.textContent = "Losses: " + lossCount;
     guessesRemain.textContent = "Guesses Left: " + guessesLeft;
     lettersGuessed.textContent = "Cookies Already Guessed: " + userGuessed;
-
 }
 
+// begin game call functions
 monsterAction();
 startGame();
